1e28a8b24ab28ac5b896148a4f679bf4463f61bb
bair-ochirov <pactuc@gmail.com>
2024-02-06 11:43:47 +0800

    feat: add styles to main section

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -2,19 +2,24 @@ import React from 'react';
 import photo from '../../../assets/images/main-photo.jpg'
 import styled from 'styled-components';
 import {FlexWrapper} from '../../../components/FlexWrapper';
+import {Container} from '../../../components/Container';
+import {theme} from '../../../styles/Theme';
 
 export const Main = () => {
     return (
         <StyledMain>
-            <FlexWrapper justify={'space-around'} align={'center'}>
-                <div>
-                    <span>Hi There</span>
-                    <Name>I am Svetlana Dyablo</Name>
-                    <MainTitle>A Web Developer. </MainTitle>
-                </div>
-
-                <Photo src={photo} alt={'mainPhoto'}/>
-            </FlexWrapper>
+            <Container>
+                <FlexWrapper justify={'space-between'} align={'center'}>
+                    <div>
+                        <SmallText>Hi There</SmallText>
+                        <Name>I am <span>Svetlana Dyablo</span></Name>
+                        <MainTitle>A Web Developer. </MainTitle>
+                    </div>
+                    <PhotoWrapper>
+                        <Photo src={photo} alt={'mainPhoto'}/>
+                    </PhotoWrapper>
+                </FlexWrapper>
+            </Container>
         </StyledMain>
     );
 };
@@ -22,6 +27,25 @@ export const Main = () => {
 const StyledMain = styled.section`
     min-height: 100vh;
     background-color: #4d75e5;
+    display: flex;
+    //align-items: center;
+`
+
+const PhotoWrapper = styled.div`
+    position: relative;
+    z-index: 0;
+    
+    &::before {
+        content: '';
+        border: 5px solid ${theme.colors.accent};
+        width: 360px;
+        height: 470px;
+
+        position: absolute;
+        top: -24px;
+        left: 24px;
+        z-index: -1;
+    }
 `
 
 const Photo = styled.img`
@@ -31,9 +55,39 @@ const Photo = styled.img`
 `
 
 const MainTitle = styled.h1`
-    
+    font-size: 27px;
+    font-weight: 400;
 `
 
 const Name = styled.h2`
-    
+    font-family: Josefin Sans, sans-serif;
+    font-size: 50px;
+    font-weight: 700;
+    margin: 10px 0;
+
+    span {
+        position: relative;
+        z-index: 0;
+
+        &::before {
+            content: '';
+            display: inline-block;
+            width: 100%;
+            height: 20px;
+            background-color: ${theme.colors.accent};
+
+            position: absolute;
+            bottom: 0;
+            z-index: -1;
+            //transform: translateY(200%);
+        }
+    }
 `
+
+const SmallText = styled.h2`
+    font-size: 14px;
+    font-weight: 400;
+`
+
+
+
