6383f9a368607a81d4f64eed7eacf2a20aedc42b
bair-ochirov <pactuc@gmail.com>
2024-02-06 12:29:11 +0800

    feat: add styles to skills section

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -1,43 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
 import {FlexWrapper} from '../../../components/FlexWrapper';
-import {Icon} from '../../../components/icons/Icon';
 import {SectionTitle} from '../../../components/SectionTitle';
 import {Skill} from './skill/Skill';
+import {Container} from '../../../components/Container';
 
 export const Skills = () => {
     return (
         <StyledSkills>
-            <SectionTitle>My Skills</SectionTitle>
-            <FlexWrapper wpar={'wrap'} justify={'space-between'}>
-                <Skill iconId={'codeSvg'}
-                       title={'html5'}
-                       description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}
-                />
-                <Skill iconId={'cssSvg'}
-                       title={'css3'}
-                       description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}
-                />
-                <Skill iconId={'reactSvg'}
-                       title={'React'}
-                       description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}
-                />
-                <Skill iconId={'styledCompSvg'}
-                       title={'styled components'}
-                       description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}
-                /><Skill iconId={'typeScriptSvg'}
-                       title={'typescript'}
-                       description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}
+            <Container>
+                <SectionTitle>My Skills</SectionTitle>
+                <FlexWrapper wpar={'wrap'} justify={'space-between'}>
+                    <Skill iconId={'codeSvg'}
+                           title={'html5'}
+                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}
+                    />
+                    <Skill iconId={'cssSvg'}
+                           title={'css3'}
+                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}
+                    />
+                    <Skill iconId={'reactSvg'}
+                           title={'React'}
+                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}
+                    />
+                    <Skill iconId={'styledCompSvg'}
+                           title={'styled components'}
+                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}
+                    /><Skill iconId={'typeScriptSvg'}
+                             title={'typescript'}
+                             description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}
                 /><Skill iconId={'figmaSvg'}
-                       title={'WEB DESIgN  '}
-                       description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}
+                         title={'WEB DESIgN  '}
+                         description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}
                 />
-            </FlexWrapper>
+                </FlexWrapper>
+            </Container>
         </StyledSkills>
     );
 };
 
 const StyledSkills = styled.section`
-    background-color: #e76a6a;
-    min-height: 100vh;
+    
 `
