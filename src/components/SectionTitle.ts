6b8f583a2844b2721cb0428775d22d22d3845704
OchirovBair <pactuc@gmail.com>
2024-02-20 19:00:03 +0800

    refactor: add adaptive to skill section

--- src/components/SectionTitle.ts
+++ src/components/SectionTitle.ts
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 import {theme} from '../styles/Theme';
+import {font} from "../styles/Common";
 
 export const SectionTitle = styled.h2`
+    ${font({family: 'Josefin Sans, sans-serif', weight: 600, Fmax: 36, Fmin:30})}
+    
     text-align: center;
-    font-family: Josefin Sans, sans-serif;
-    font-size: 36px;
-    font-weight: 600;
     letter-spacing: 5px;
-    margin-bottom: 90px;
+    margin-bottom: 80px;
     
     position: relative;
     
@@ -23,6 +23,13 @@ export const SectionTitle = styled.h2`
         transform: translateX(-50%);
         bottom: -30px;
 
+        @media ${theme.media.mobile} {
+            bottom: -24px;
+        }
+    }
+    
+    @media ${theme.media.mobile} {
+        margin-bottom: 45px;
     }
 `
 
